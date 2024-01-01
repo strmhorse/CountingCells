b8c2b77a83f6f67bff6e10ebb65f3e512f9a1903
scott <strmhorse@gmail.com>
2024-01-01 17:18:11 -0500

    Add cells obj

--- countingpy/cells_object.py
+++ countingpy/cells_object.py
@@ -1,7 +1,74 @@
 #!/usr/bin/python3
-#
-#  cells_object.py
-#
-#  Object to hold and abstract details about the Cells object
-#
-#
+"""
+cells_object.py
+
+Object to hold and abstract details about the Cells object
+
+"""
+
+from collections import namedtuple, defaultdict
+
+def constant_factory(value):
+    return lambda: value
+
+class Point = namedtuple('Point', ['y', 'x'], defaults=[0, 0])
+
+class CellsObject:
+    """
+    """
+
+    def __init__(self):
+        """
+        Create the default values dictionary.
+        It uses the constant_factory to return 0 for any non-set value
+        This simulates a sparse array
+        """
+        self._values = defaultdict(constant_factory(0))
+
+    def validate_index(self, this_height, this_width):
+        """
+        Raises the InvalidIndexException if a value is beyond the set bounds.
+        """
+        if (this_height > self._height) or (this_width > self._width):
+            raise InvalidIndexException(this_height, this_width, self._height, self._width)
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, inval):
+        if (inval <= 0):
+            raise InvalidCountingArgument("height", inval)
+        self._height = inval
+
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, inval):
+        if (inval <= 0):
+            raise InvalidCountingArgument("width", inval)
+        self._width = inval
+
+    @property
+    def point(self, out_height, out_width):
+        """
+        Getter for point
+        """
+        self.validate_index(out_height, out_width)
+        return self._values(Point(out_height, out_width))
+
+    def set_point(self, in_height, in_width, in_value):
+        """
+        Setter for point
+        TODO:  Value check the in_value
+        """
+        self.validate_index(in_height, in_width)
+        self._values(Point(in_height, in_width)) = in_value
+
+    point = property(set_point, get_point, del_point)
+
+
+
--- countingpy/counting_errors.py
+++ countingpy/counting_errors.py
@@ -17,4 +17,17 @@ class InvalidCountingArgument(Exception):
         self.message = f"Need a {argname} greater than {minvalue}, you gave {argvalue}."
         super().__init__(self.message)
 
+class InvalidIndexException(Exception):
+    """
+    Raised when a given point is beyond the maximum area values.
+    """
+    def __init__(self, in_height, in_width, obj_height, obj_width):
+        self.in_height = in_height
+        self.in_width = in_width
+        self.obj_height = obj_height
+        self.obj_width = obj_width
+        self.message = f"Your index ({in_height}, {in_width}) exceeded the "
+                        "maximum value of ({obj_height}, {obj_width})."
+        super().__init__(self.message)
+
 
